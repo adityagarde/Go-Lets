4694cc70a8aab8c218a3fe0f86e3c035f4651bf8
adityagarde <aditya.garde96@gmail.com>
2022-02-04 02:06:00 +0530

    Simple Nodejs API server to talk

---
+++ mywebserver/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to simple Node API Utility")
+})
+
+app.get('/get', (req, res) => {
+    res.status(200).json({ message: "Hello to everyone!" })
+})
+
+app.post('/post', (req, res) => {
+    let myJson = req.body;      // your JSON
+
+    res.status(200).send(myJson);
+})
+
+app.post('/postform', (req, res) => {
+    res.status(200).send(JSON.stringify(req.body));
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
